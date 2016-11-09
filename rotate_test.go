685eb10d7d981499bd277bfaa841cd289fb511b6
alex cai <caiyingyao@ibbd.net>
2016-11-09 22:30:08 +0800

    测试

--- rotate_test.go
+++ rotate_test.go
@@ -2,6 +2,7 @@ package rotateFile
 
 import (
 	"testing"
+	"time"
 )
 
 func TestWrite(t *testing.T) {
@@ -12,6 +13,11 @@ func TestWrite(t *testing.T) {
 	_, err := file.WriteString("hello world")
 	if err != nil {
 		t.Fatal(err)
+	}
+
+	// 模拟日期切换
+	nowFunc = func() time.Time {
+		return time.Now().Add(time.Hour * 25)
 	}
 
 	_, err = file.WriteString("hello world2")
